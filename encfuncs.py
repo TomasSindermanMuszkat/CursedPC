379d0b938f4b7b9db1b87098d84767db81b915bb
DrownNotably <61554232+DrownNotably@users.noreply.github.com>
2020-10-19 17:44:54 +0100

    decrypt_file() implemented.

--- encfuncs.py
+++ encfuncs.py
@@ -12,6 +12,14 @@ def encrypt_file(filepath):
         fwrite.write(crypt.encrypt(data))
     os.rename(filepath, filepath+".enc")
 
+def decrypt_file(filepath):
+    assert(filepath.endswith(".enc"))
+    with open(filepath, "rb") as fread:
+        data = fread.read()
+    with open(filepath, "wb") as fwrite:
+        fwrite.write(crypt.decrypt(data))
+    os.rename(filepath, filepath[:-4])
+
 def encrypt_dir(root):
     files = glob.glob(root + "/*", recursive=True)
     # Filter out files that are already encrypted, else it will make
